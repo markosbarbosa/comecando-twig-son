3a84745ee3ec4c8ba45d54d308e8be3b5e9ccfc3
Marcos Barbosa <barbosa.mg@gmail.com>
2017-01-26 22:36:31 -0200

    Acrescentando exemplos de convenções de extensões dos templates

--- public/index.php
+++ public/index.php
@@ -14,14 +14,15 @@ require __DIR__ . '/../vendor/autoload.php';
 
 // require __DIR__ . '/../src/app.php';
 
-$loader = new Twig_Loader_Array([
-	'index' => 'Helo World!!! {{name}}'
-]);
+// $loader = new Twig_Loader_Array([
+// 	'index' => 'Helo World!!! {{name}}'
+// ]);
 
-
+			          
+$loader = new Twig_Loader_Filesystem(__DIR__.'/../twig_templates');
 
 
 $twig = new Twig_Environment($loader);
 
 
-echo $twig->render('index', [name=>'Marcos']);
+echo $twig->render('index.html.twig', [name=>'Marcos Barbosa']);
